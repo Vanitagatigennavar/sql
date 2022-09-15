954e13137023a4b7d90e5db83b2acbcf5100c700
Vanitagatigennavar <xworkzvanita@gmail.com>
2022-09-15 11:56:34 +0530

    sql program

---
+++ program2.sql
@@ -0,0 +1,9 @@
+
+CREATE DATABASE xworkzodc;
+use xworkzodc;
+CREATE TABLE `xworkzodc`.`vanita` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+  insert into vanita values(1,'vasu');
+SELECT * FROM xworkzodc.vanita;
