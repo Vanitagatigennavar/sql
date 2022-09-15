4227af98a833a1723b736f1d465041abdbaa0df5
Vanitagatigennavar <xworkzvanita@gmail.com>
2022-09-15 11:24:32 +0530

    sql programs

---
+++ program1.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE xworkzodc;
+use xworkzodc;
+CREATE table student (id int,name varchar(20),usn_no bigint);
+
+insert into  student values(1,'vanita',124567);
+select*FROM student;
+
+desc student;
+insert into student values(2,'nayana',1234567);
+
+insert into student(id,name)values(3,'vasu');
+
+
+
+
