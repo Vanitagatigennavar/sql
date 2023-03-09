4da3e6d9df611e9e8c0084f85b5c4846400ac784
Vanitagatigennavar <xworkzvanita@gmail.com>
2023-03-10 00:32:20 +0530

    sql Task

---
+++ Mall.sql
@@ -0,0 +1,2 @@
+SELECT * FROM shoppingmall.mall;
+INSERT INTO shoppingmall.mall VALUES(4,'UB CITY','Vinay','@Vinay','1','unlocked','vanita21092000@gmail.com',0,'','Nayana','2023-01-8','Rajani','2023-01-10',1,'');
